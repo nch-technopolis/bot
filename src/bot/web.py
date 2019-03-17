c9c3c46842cbfedfe22759f4bd72dd2e0013351b
Rustam Kashapov <hardtechnik91@gmail.com>
2019-03-17 18:26:14 +0300

    remove logging

--- src/bot/web.py
+++ src/bot/web.py
@@ -1,25 +1,9 @@
-import functools
-import json
-from urllib.parse import urljoin
+from bottle import post, get
 
-from bottle import post, get, request
-
-from .api import TelegramBotAPI
-from .config import API_TOKEN, LOG_FILE_PATH, VERSION, WEBHOOK_PATH
-
-
-def log_request(view):
-    @functools.wraps(view)
-    def wrapper(*args, **kwargs):
-        with open(LOG_FILE_PATH, 'a') as f:
-            f.write(json.dumps(request.params))
-            f.wirte('\n')
-        return view(*args, **kwargs)
-    return wrapper
+from .config import VERSION, WEBHOOK_PATH
 
 
 @post(WEBHOOK_PATH)
-@log_request
 def webhook():
     return 'OK'
 
