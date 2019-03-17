ccbba6d05416a261cbd8f962e28a98cdda1c6d5e
Rustam Kashapov <hardtechnik91@gmail.com>
2019-03-17 21:29:53 +0300

    added first reaction

--- src/bot/api.py
+++ src/bot/api.py
@@ -12,13 +12,17 @@ class TelegramBotAPI:
     def __init__(self, token):
         self.token = token
 
-    def _request(self, method, **kwargs):
+    def _request(self, method, **kwargs) -> dict:
         url = f'{API_ENDPOINT}/bot{self.token}/{method}'
         return requests.post(url, **kwargs).json()
 
-    def set_webhook(self, url: str, allowed_updates: List[AnyStr] = None) -> dict:
+    def set_webhook(self, url: str, allowed_updates: List[AnyStr] = None):
         params = {'url': url, 'allowed_updates': allowed_updates}
         return self._request('setWebhook', data=params)
 
     def delete_webhook(self):
         return self._request('deleteWebhook')
+
+    def send_message(self, *, chat_id, text):
+        return self._request('sendMessage', chat_id=chat_id, text=text)
+
--- src/bot/web.py
+++ src/bot/web.py
@@ -1,10 +1,20 @@
 from bottle import post, get
 
-from .config import VERSION, WEBHOOK_PATH
+from .api import TelegramBotAPI
+from .config import API_TOKEN, VERSION, WEBHOOK_PATH
 
 
 @post(WEBHOOK_PATH)
 def webhook():
+    update = request.json
+    if 'message' in update:
+        message = update['message']
+        text = message['text']
+        if 'как дела' in text.lower():
+            chat_id = message['chat']['id']
+            message = 'Ленар меня предал'
+            api = TelegramBotAPI(API_TOKEN)
+            api.send_message(chat_id=chat_id, text=text)
     return 'OK'
 
 
