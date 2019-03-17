6488bfec8233401c0809a5920f54bdbd2f72debc
Rustam Kashapov <hardtechnik91@gmail.com>
2019-03-17 22:13:53 +0300

    added sentry integration

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
+import sentry_sdk
 import bottle
 
 from bot import web
 
-application = bottle.default_app()
-
 if __name__ == '__main__':
     bottle.run(host='localhost', port=9000, debug=True)
+else:
+    sentry_sdk.init("https://f7ebbcbe9cfe4d3fbbc86b353081003b@sentry.io/1417397")
+    application = bottle.default_app()
 
