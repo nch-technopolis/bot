48d11692a81663a90296cd58947e1e0f3cca5f24
Rustam Kashapov <hardtechnik91@gmail.com>
2019-03-17 22:24:31 +0300

    enabled sentry wsgi middleware

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import sentry_sdk
 import bottle
 
 from bot import web
@@ -6,6 +5,9 @@ from bot import web
 if __name__ == '__main__':
     bottle.run(host='localhost', port=9000, debug=True)
 else:
+    import sentry_sdk
+    from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+
     sentry_sdk.init("https://f7ebbcbe9cfe4d3fbbc86b353081003b@sentry.io/1417397")
-    application = bottle.default_app()
+    application = SentryWsgiMiddleware(bottle.default_app())
 
