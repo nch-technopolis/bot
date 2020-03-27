0c57f874e2c586fbd2c97f6a2530af4cd99cbd95
Rustam Kashapov <hardtechnik@yandex.ru>
2020-03-27 19:54:48 +0300

    added covid-19 message

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,3 +1,4 @@
+import random
 import datetime
 import xml.etree.ElementTree as ET
 
@@ -20,6 +21,24 @@ def get_dollar_rate(date):
             return currency.find('Value').text
 
 
+def tell_tale():
+    parts = [
+        ['Внимение,', 'Срочно,', 'Привет, ребят,'],
+        ['мне', 'нам'],
+        ['тут только что'],
+        ['позвонил', 'написал'],
+        ['знакомый из министерства', 'знакомый хирург', 'брат', 'дядя', 'отец', 'родственник из Сибири'],
+        ['и попросил'],
+        ['никому не говорить', 'рассказать всем'],
+        ['о том, что реальная информация'],
+        ['скрывается', 'преувеличивается'],
+        ['и в скором времени'],
+        ['пизда.', 'не пизда.'],
+        ['\nhttps://vademec.ru/upload/iblock/c5a/c5a908fcfd36b308bc55686202c881d6.jpg'],
+    ]
+    return ' '.join(random.choice(part) for part in parts)
+
+
 def reply(update):
     chat_id = update['message']['chat']['id']
     text = update['message'].get('text')
@@ -33,3 +52,5 @@ def reply(update):
         except:
             message = 'Не знаю'
         telegram.send_message(chat_id=chat_id, text=message)
+    elif text == 'Денис, что слышно про коронавирус?':
+        telegram.send_message(chat_id=chat_id, text=tell_tale())
